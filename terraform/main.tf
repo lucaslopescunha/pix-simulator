21e3ec69018ac734d1f74e555f2c3f948d73f40f
lucaslopescunha <lucaslopescunha@gmail.com>
2026-05-07 20:25:52 -0300

    Changing from eks to fargate

--- terraform/main.tf
+++ terraform/main.tf
@@ -179,6 +179,11 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+resource "aws_cloudwatch_log_group" "ecs_logs" {
+    name = "/ecs/pix-simulator"
+    retention_in_days = 7
+}
+
 #################################################
 # 7. Task Definition
 #################################################
@@ -206,6 +211,14 @@ resource "aws_ecs_task_definition" "app" {
           protocol      = "tcp"
         }
       ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group = "/ecs/pix-simulator"
+          awslogs-region = "us-east-1"
+          awslogs-stream-prefix = "ecs"
+        }
+      }
     }
   ])
 }
